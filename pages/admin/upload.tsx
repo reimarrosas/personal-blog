61f6451483b681c93754beeb3cf99e900e9c7d40
Reimar Rosas <reimarrosas0208@gmail.com>
2021-12-17 00:15:39 -0500

    Implemented the barebones of the upload page.

---
+++ pages/admin/upload.tsx
@@ -0,0 +1,63 @@
+import { FormEvent, MutableRefObject, useRef, useState } from 'react';
+import Container from '../../components/Container';
+import { Width } from '../../utils/types';
+
+const Upload: React.FC = () => {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const mdFile = useRef<HTMLInputElement>(null);
+
+  const handleId = (evt: FormEvent<HTMLInputElement>) =>
+    setId(evt.currentTarget.value);
+  const handlePw = (evt: FormEvent<HTMLInputElement>) =>
+    setPw(evt.currentTarget.value);
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    console.log(id, pw, mdFile.current?.files);
+  };
+
+  return (
+    <main>
+      <Container center={true} size={Width.MD}>
+        <form onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <label htmlFor='id'>Admin ID: </label>
+            <input
+              value={id}
+              onChange={handleId}
+              type='text'
+              name='id'
+              id='id'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='pw'>Admin PW: </label>
+            <input
+              value={pw}
+              onChange={handlePw}
+              type='text'
+              name='pw'
+              id='pw'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='mdFile'>Markdown File: </label>
+            <input
+              ref={mdFile}
+              type='file'
+              name='mdFile'
+              id='mdFile'
+              accept='md'
+            />
+          </div>
+          <div className='form-group'>
+            <input type='submit' value='Upload' />
+          </div>
+        </form>
+      </Container>
+    </main>
+  );
+};
+
+export default Upload;
